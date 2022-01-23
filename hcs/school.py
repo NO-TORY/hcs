a9842d1980f14e6a7d682d90882192fc59a49490
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-23 14:25:52 +0900

    Update school.py

--- hcs/school.py
+++ hcs/school.py
@@ -1,12 +1,14 @@
 from .hcs import Route
+from .models import SearchSchool
 
-class SearchSchool:
-    def __init__(self, lctnScCode: str, schulCrseScCode: str, orgName: str, loginType: str = "school"):
-        response = Route("GET", "https://hcs.eduro.go.kr", f"/v2/searchSchool?lctnScCode={lctnScCode}&schulCrseScCode={schulCrseScCode}&orgName={orgName}&loginType={loginType}")
+def searchSchool(lctnScCode: str, schulCrseScCode: str, orgName: str, loginType: str = "school"):
+    response = Route("GET", "https://hcs.eduro.go.kr", f"/v2/searchSchool?lctnScCode={lctnScCode}&schulCrseScCode={schulCrseScCode}&orgName={orgName}&loginType={loginType}")
         
-        if len(response.response.json()["schulList"]) > 1:
-            raise IndexError("너무 많은 학교가 검색 되었습니다.")
+    if len(response.response.json()["schulList"]) > 1:
+        raise IndexError("너무 많은 학교가 검색 되었습니다.")
 
-        self.school = response.response.json()["schulList"][0]
-        self.orgCode = self.school.get("orgCode")
-        self.atptOfcdcConctUrl = self.school.get("atptOfcdcConctUrl")
+    school = response.response.json()["schulList"][0]
+    orgCode = school.get("orgCode")
+    atptOfcdcConctUrl = school.get("atptOfcdcConctUrl")
+
+    return SearchSchool(atptOfcdcConctUrl=atptOfcdcConctUrl, orgCode=orgCode)
