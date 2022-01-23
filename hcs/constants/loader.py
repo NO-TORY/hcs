9bfa9315ea971521b2b9f25a94addbb677316291
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-23 14:24:51 +0900

    Update loader.py

--- hcs/constants/loader.py
+++ hcs/constants/loader.py
@@ -2,6 +2,9 @@ from pkgutil import get_data
 from json import loads
 from typing import Final
 
-levels: Final[dict] = loads(get_data("hcs.constants.loader", "levels.json").decode("utf-8"))
-regions: Final[dict] = loads(get_data("hcs.constants.loader", "regions.json").decode("utf-8"))
-answer: dict = loads(get_data("hcs.constants.loader", "answer.json").decode("utf-8"))
+from_path = lambda package, path: get_data(package, path)
+json_decode = lambda data: loads(data.decode())
+
+levels: Final[dict] = json_decode(from_path("hcs.constants.loader", "levels.json"))
+regions: Final[dict] = json_decode(from_path("hcs.constants.loader", "regions.json"))
+answer: dict = json_decode(from_path("hcs.constants.loader", "answer.json"))
