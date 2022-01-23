5b1ba9c5d5490e2d456bbbc13869d4f8caa3f0e2
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-23 14:25:07 +0900

    Update filter.py

--- hcs/constants/filter.py
+++ hcs/constants/filter.py
@@ -1,7 +1,5 @@
 from .loader import levels, regions
 
-import re
-
 def levelFilter(word):
     if "유" in word:
         return levels["유등"]
@@ -16,9 +14,56 @@ def levelFilter(word):
         return levels["특수"]
 
 def regionFilter(word):
-    region = ",".join(regions)
+    if "서울" in word:
+        return regions["서울"]
 
-    word = re.search(word, region).group()
+    if "부산" in word:
+        return regions["부산"]
 
-    return regions[re.sub(fr"[^{word}]", "", region)]
+    if "대구" in word:
+        return regions["대구"]
 
+    if "인천" in word:
+        return regions["인천"]
+
+    if "광주" in word:
+        return regions["광주"]
+
+    if "대전" in word:
+        return regions["대전"]
+
+    if "울산" in word:
+        return regions["울산"]
+
+    if "세종" in word:
+        return regions["세종"]
+
+    if "경기" in word:
+        return regions["경기"]
+    
+    if "강원" in word:
+        return regions["강원"]
+
+    if "충" in word:
+        if "북" in word:
+            return regions["충북"]
+
+        if "남" in word:
+            return regions["충남"]
+
+    if "전" in word:
+        if "북" in word:
+            return regions["전북"]
+
+        if "남" in word:
+            return regions["전남"]
+
+    if "경" in word:
+        if "북" in word:
+            return regions["경북"]
+
+        if "남" in word:
+            return regions["경남"]
+
+    if "제주" in word:
+        return regions["제주"]
