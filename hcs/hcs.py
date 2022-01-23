03d6d4dddb838243a3430c0830c7a582ac384272
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-23 21:08:28 +0900

    Update hcs.py

--- hcs/hcs.py
+++ hcs/hcs.py
@@ -6,3 +6,4 @@ SESSION = requests.Session()
 class Route:
     def __init__(self, method: Literal["GET", "POST"], atptOfcdcConctUrl, endpoint, **kwargs):
         self.response = SESSION.request(method, atptOfcdcConctUrl + endpoint if "https://" in atptOfcdcConctUrl else "https://" + atptOfcdcConctUrl + endpoint, **kwargs)
+        self.response.raise_for_status()
