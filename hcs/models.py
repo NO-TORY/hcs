3d1463f334c42151d7b91e055f29233c88cacd1f
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-23 14:24:14 +0900

    Create models.py

---
+++ hcs/models.py
@@ -0,0 +1,22 @@
+class FindUser:
+    def __init__(self, token):
+        self.token = token
+
+class Validate:
+    def __init__(self, token):
+        self.token = token
+
+class GetUserInfo:
+    def __init__(self, token):
+        self.token = token
+
+class SearchSchool:
+    def __init__(self, atptOfcdcConctUrl, orgCode):
+        self.atptOfcdcConctUrl = atptOfcdcConctUrl
+        self.orgCode = orgCode
+
+class Login:
+    def __init__(self, token, atptOfcdcConctUrl, orgCode):
+        self.token = token
+        self.atptOfcdcConctUrl = atptOfcdcConctUrl
+        self.orgCode = orgCode
