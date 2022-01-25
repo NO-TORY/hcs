4eececaa4cf5b05a21ce86aed6ff5a0f13745c00
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-25 13:56:33 +0900

    Update hcs.py

--- hcs/hcs.py
+++ hcs/hcs.py
@@ -3,13 +3,17 @@ from __future__ import annotations
 import requests
 
 try:
-    from typing import Literal
+    from typing import Literal, TYPE_CHECKING, Any
 except:
-    from typing_extensions import Literal
+    from typing_extensions import Literal, TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from _typeshed import Self
 
 SESSION = requests.Session()
 
 class Route:
-    def __init__(self, method: Literal["GET", "POST"], atptOfcdcConctUrl, endpoint, **kwargs):
+    def __init__(self, method, atptOfcdcConctUrl, endpoint, **kwargs):
+        # type: (Self, Literal["GET", "POST"], Any, str, Any) -> None
         self.response = SESSION.request(method, atptOfcdcConctUrl + endpoint if "https://" in atptOfcdcConctUrl else "https://" + atptOfcdcConctUrl + endpoint, **kwargs)
         self.response.raise_for_status()
