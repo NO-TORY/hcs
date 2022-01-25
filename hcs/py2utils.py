1c920cb80081580b7f0436c36efb09b575e1c333
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-25 21:21:58 +0900

    Update py2utils.py

--- hcs/py2utils.py
+++ hcs/py2utils.py
@@ -4,6 +4,7 @@ from json import load
 from io import open
 from typing import TYPE_CHECKING
 from .__main__ import selfcheck
+from .models import Result
 
 from sys import version
 
@@ -13,5 +14,6 @@ if not version.startswith("2"):
 if TYPE_CHECKING:
     from _typeshed import StrOrBytesPath
 
-def selfcheck_from_json(file: "StrOrBytesPath"):
+def selfcheck_from_json(file):
+    # type: ("StrOrBytesPath") -> Result
     return selfcheck(**load(open(file, encoding="utf-8")))
