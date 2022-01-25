0a082e42ca1740e2aef30d0ae30aebb55812bd50
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-25 22:20:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,15 +21,16 @@ packages = [
 ]
 
 setup(
-    name="py-hcs",
-    version=open("hcs/version").read(),
-    author="노토리",
-    description="자가진단 라이브러리.",
-    long_description_content_type="text/markdown",
-    long_description=open("README.md", encoding="utf-8").read(),
-    packages=packages,
-    install_requires=requirements,
-    extra_requires=requirements_extra,
-    include_package_data=True,
-    python_requires=">=3"
+    name                          = "py-hcs",
+    version                       = open("hcs/version").read(),
+    url                           = "https://github.com/oxsixcseven/hcs",
+    author                        = "노토리",
+    description                   = "자가진단 라이브러리.",
+    long_description_content_type = "text/markdown",
+    long_description              = open("README.md", encoding="utf-8").read(),
+    packages                      = packages,
+    install_requires              = requirements,
+    extra_requires                = requirements_extra,
+    include_package_data          = True,
+    python_requires               = ">=3",
 )
