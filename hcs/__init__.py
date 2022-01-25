041a80fd2dbff2c2531e382e8bec3aa9f7db208a
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-25 13:47:40 +0900

    Update __init__.py

--- hcs/__init__.py
+++ hcs/__init__.py
@@ -1,2 +1,7 @@
-from hcs.__main__ import selfcheck, token_selfcheck
+from hcs.__main__ import (
+    selfcheck,
+    token_selfcheck,
+    load_from_token,
+    load_from_token_file,
+)
 from hcs.const import __version__
