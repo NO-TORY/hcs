2861a1a0bcb35048b981312bc76e1fb43d6336ac
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-25 21:26:30 +0900

    Update py2utils.py

--- hcs/py2utils.py
+++ hcs/py2utils.py
@@ -16,4 +16,20 @@ if TYPE_CHECKING:
 
 def selfcheck_from_json(file):
     # type: ("StrOrBytesPath") -> Result
+    """json 파일에서 정보를 불러와 자가진단을 합니다. (파이썬 2 전용)
+    
+    json 파일 예제: 
+
+    ```json
+    {
+        "name": "홍길동",
+        "birth": "생년월일 6자리",
+        "area": "거주 지역",
+        "school_name": "학교명",
+        "level": "학교 급 (예: 초, 중, 고)",
+        "password": "비밀번호",
+        "save_token": true "(기본값: false)"
+    }
+    ```
+    """
     return selfcheck(**load(open(file, encoding="utf-8")))
