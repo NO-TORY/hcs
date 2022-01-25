fb6304bd6c37563238903cd19d6a6cc6cfa2fb8c
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-25 21:15:09 +0900

    Update school_py2.py

--- hcs/school_py2.py
+++ hcs/school_py2.py
@@ -7,7 +7,7 @@ def searchSchool(lctnScCode, schulCrseScCode, orgName, loginType = "school"):
     response = Route("GET", "https://hcs.eduro.go.kr", f"/v2/searchSchool?lctnScCode={lctnScCode}&schulCrseScCode={schulCrseScCode}&orgName={orgName}&loginType={loginType}")
         
     if len(response.response.json()["schulList"]) > 1:
-        raise IndexError("너무 많은 학교가 검색 되었습니다.")
+        raise IndexError("Too many schools are searched.")
 
     school = response.response.json()["schulList"][0]
     orgCode = school.get("orgCode")
